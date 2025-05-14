19f1a850b855c04c6fc94b0871781df5f522f2ce
JoseDavid-gif <armijosjosedavid@gmail.com>
2025-05-14 04:24:31 -0500

    cambio 2

--- ejemplo1/agregar_data2.py
+++ ejemplo1/agregar_data2.py
@@ -1,19 +1,23 @@
 from sqlalchemy.orm import sessionmaker
-
-from crear_base import Saludo
+from crear_base import Saludo2
 from configuracion import engine
-
 import pandas as pd
 
+# Crear la sesión
 Session = sessionmaker(bind=engine)
 session = Session()
 
-miSaludo2 = Saludo()
-miSaludo2.mensaje = "Buenas tardes"
-miSaludo2.tipo = "formal"
+# Leer el CSV
+df = pd.read_csv("./data/saludos_mundo.csv", sep='|')
 
+# Iterar sobre cada fila del DataFrame
+for index, row in df.iterrows():
+    miSaludo2 = Saludo2()
+    miSaludo2.mensaje = row['saludo']
+    miSaludo2.tipo = row['tipo']
+    miSaludo2.origen = row['origen']
+    
+    session.add(miSaludo2)
 
-
-df = pd.read_csv("./data/saludos_mundo.csv")
-
-print(df)
+# Guardar los cambios en la base de datos
+session.commit()
